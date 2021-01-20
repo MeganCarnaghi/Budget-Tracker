31ce39bc55b89ce4a0076797215478e7236ee72f
Megan <carnagh5@gmail.com>
2021-01-19 21:39:16 -0500

    Updated server

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //Connect to the database
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
